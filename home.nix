ae1dd56248b34afe3ea1f76443546b5b502e8b3f
David Torralba Goitia <david.torralba.goitia@gmail.com>
2025-02-07 09:13:27 +0000

    toggl-extractor: add systemd service and timer to run daily

--- home.nix
+++ home.nix
@@ -81,4 +81,35 @@
     userName = "David Torralba Goitia";
     userEmail = "david.torralba.goitia@gmail.com";
   };
+
+  systemd.user.services = {
+    toggl-extractor = {
+      Unit = {
+        Description = "tool to download all your Toggl data using the Toggl API";
+        After = ["network.target"];
+      };
+      Service = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.nix}/bin/nix run .#start";
+        WorkingDirectory = "${config.home.homeDirectory}/projects/toggl-extractor";
+        EnvironmentFile = "${config.home.homeDirectory}/.secrets/toggl-extractor";
+      };
+      Install.WantedBy = ["default.target"];
+    };
+  };
+
+  systemd.user.timers = {
+    toggl-extractor-daily = {
+      Unit = {
+        Description = "run toggl-extractor daily";
+      };
+      Timer = {
+        OnCalendar = "06:00";
+        Unit = "toggl-extractor.service";
+      };
+      Install = {
+        WantedBy = ["timers.target"];
+      };
+    };
+  };
 }
