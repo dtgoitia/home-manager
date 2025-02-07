46979159af7a4b5598001da3474e96f46bb9a205
David Torralba Goitia <david.torralba.goitia@gmail.com>
2025-02-07 22:57:19 +0000

    git: add fish abbreviations

--- modules/git.nix
+++ modules/git.nix
@@ -4,9 +4,32 @@
   ...
 }: {
   config = lib.mkIf config.programs.git.enable {
-    programs.git = {
-      userName = "David Torralba Goitia";
-      userEmail = "david.torralba.goitia@gmail.com";
+    programs = {
+      git = {
+        userName = "David Torralba Goitia";
+        userEmail = "david.torralba.goitia@gmail.com";
+      };
+
+      fish = lib.mkIf config.programs.fish.enable {
+        shellAbbrs = {
+          g = "git";
+          gs = "git status -s";
+          ga = "git add";
+          gc = "git commit";
+          gpl = "git pull";
+          gps = "git push";
+          gch = "git checkout";
+          gb = "git branch";
+          gd = "git branch -D";
+          gm = "git branch -m";
+          gl = "git log --oneline --graph --decorate --branches";
+          gr = "git reset HEAD~";
+          gw = ''git commit -am "WIP_UNDO_ME"'';
+          grc = "git rebase --continue";
+          gra = "git rebase --abort";
+          gf = "git fetch --all --prune";
+        };
+      };
     };
   };
 }
