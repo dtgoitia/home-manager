844419968c760646706827bf6f3fa28b64be1fee
David Torralba Goitia <david.torralba.goitia@gmail.com>
2025-02-18 08:25:39 +0000

    fish: add Python functions and abbreviations

--- modules/fish.nix
+++ modules/fish.nix
@@ -7,6 +7,24 @@
     programs.fish = {
       shellAbbrs = {
         rmd = "rm -rf";
+
+        ac = ". (find_venv)/bin/activate.fish";
+        de = "deactivate";
+      };
+
+      functions = {
+        find_venv = {
+          body = ''
+            for dir_name in ".venv" ".env" "venv"
+              if test -e $dir_name
+                echo "$dir_name"
+                break
+                return 0
+              end
+            end
+          '';
+          description = "find Python virtual environment";
+        };
       };
     };
   };
