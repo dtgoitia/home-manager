daf20842e39c542d75bdc46ccb6864140bbb7305
David Torralba Goitia <david.torralba.goitia@gmail.com>
2025-02-22 09:27:31 +0000

    beltz: install `k9s`

--- hosts/beltz.nix
+++ hosts/beltz.nix
@@ -11,6 +11,7 @@
     fzf.enable = true;
     jq.enable = true;
     jump.enable = true;
+    k9s.enable = true;
     keepassxc.enable = true;
     ledger-cli.enable = true;
     meld.enable = true;
