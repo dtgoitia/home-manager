2472b00d06b0ac362c6b79e8077386164b87e3b1
David Torralba Goitia <david.torralba.goitia@gmail.com>
2025-02-23 11:54:19 +0000

    modules(keepassxc): support `*.desktop`

--- modules/keepassxc.nix
+++ modules/keepassxc.nix
@@ -3,7 +3,18 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  # fix `.desktop` file: use absolute paths
+  desktop_file_in_derivation = builtins.readFile "${pkgs.keepassxc}/share/applications/org.keepassxc.KeePassXC.desktop";
+  patch__Exec = "Exec=${pkgs.keepassxc}/bin/keepassxc %f";
+  patch__TryExec = "TryExec=${pkgs.keepassxc}/bin/keepassxc";
+  patch__Icon = "Icon=${pkgs.keepassxc}/share/icons/hicolor/scalable/apps/keepassxc.svg";
+  patched_desktop_file =
+    builtins.replaceStrings
+    ["Exec=keepassxc %f" "TryExec=keepassxc" "Icon=keepassxc"]
+    [patch__Exec patch__TryExec patch__Icon]
+    desktop_file_in_derivation;
+in {
   options.programs.keepassxc = {
     enable = lib.mkOption {
       type = lib.types.bool;
@@ -17,5 +28,13 @@
     home.packages = [
       pkgs.keepassxc
     ];
+
+    home.file.".local/share/applications/org.keepassxc.KeePassXC.desktop".text = patched_desktop_file;
+
+    home.activation = {
+      update-desktop-database = lib.hm.dag.entryAfter ["writeBoundary"] ''
+        ${pkgs.desktop-file-utils}/bin/update-desktop-database ~/.local/share/applications/
+      '';
+    };
   };
 }
