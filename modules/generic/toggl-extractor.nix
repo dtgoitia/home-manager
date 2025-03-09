7bbd4eba61f458d583d4b8b26c9fcda55b2642a6
David Torralba Goitia <david.torralba.goitia@gmail.com>
2025-03-09 14:51:57 +0000

    modules(toggl-extractor): support config

--- modules/generic/toggl-extractor.nix
+++ modules/generic/toggl-extractor.nix
@@ -12,9 +12,22 @@
       example = true;
       description = "systemd service and timer to run toggl-extractor every day (toggl-extractor must be installed separately)";
     };
+
+    data_dir = lib.mkOption {
+      type = lib.types.str;
+      default = "~/.local/share/toggl-extractor";
+      example = "/your/preferred/path";
+      description = "directory where the downloaded data will be stored";
+    };
   };
 
   config = lib.mkIf config.services.toggl-extractor.enable {
+    home.file.".config/toggl-extractor/config.json".text = ''
+      {
+        "data_dir": "${config.services.toggl-extractor.data_dir}"
+      }
+    '';
+
     systemd.user.services = {
       toggl-extractor = {
         Unit = {
